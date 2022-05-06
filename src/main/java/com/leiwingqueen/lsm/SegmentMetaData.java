5a4ba512f7cd11884f867a468e2eca21111b2de9
leiwingqueen <leiwingqueen@qq.com>
2022-05-06 15:06:37 +0800

    metaData修改

---
+++ src/main/java/com/leiwingqueen/lsm/SegmentMetaData.java
@@ -0,0 +1,45 @@
+package com.leiwingqueen.lsm;
+
+import java.nio.ByteBuffer;
+
+public class SegmentMetaData {
+    public static final int META_DATA_SIZE = 8 + 4 + 8 + 4;
+    private long dataOffset;
+    private int dataLen;
+    private long indexOffset;
+    private int indexLen;
+
+    private SegmentMetaData() {
+    }
+
+    public SegmentMetaData(long dataOffset, int dataLen, long indexOffset, int indexLen) {
+        this.dataOffset = dataOffset;
+        this.dataLen = dataLen;
+        this.indexOffset = indexOffset;
+        this.indexLen = indexLen;
+    }
+
+    public static SegmentMetaData readBytes(byte[] arr) {
+        SegmentMetaData metaData = new SegmentMetaData();
+        ByteBuffer buffer = ByteBuffer.wrap(arr);
+        int offset = 0;
+        metaData.dataOffset = buffer.getLong(offset);
+        offset += 8;
+        metaData.dataLen = buffer.getInt(offset);
+        offset += 4;
+        metaData.indexOffset = buffer.getLong(offset);
+        offset += 8;
+        buffer.getInt(offset);
+        return metaData;
+    }
+
+
+    public byte[] toByteArray() {
+        ByteBuffer buffer = ByteBuffer.allocate(META_DATA_SIZE);
+        buffer.putLong(dataOffset);
+        buffer.putInt(dataLen);
+        buffer.putLong(indexOffset);
+        buffer.putInt(indexLen);
+        return buffer.array();
+    }
+}
